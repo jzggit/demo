574d291289a2aa69b377b563e23aa9d52f49732a
jzg <1805206076@qq.com>
2020-03-24 13:55:18 +0800

    用户表修改

--- src/main/java/com/jiang/demo/sys/user/entity/SysUserInfo.java
+++ src/main/java/com/jiang/demo/sys/user/entity/SysUserInfo.java
@@ -2,9 +2,26 @@ package com.jiang.demo.sys.user.entity;
 
 import lombok.Data;
 
+import javax.persistence.Id;
+import java.util.Date;
+
 @Data
 public class SysUserInfo {
+    @Id
     private Long sysUserId;
     private String sysUserName;
     private Integer sysUserAge;
+    private Integer sysUserSex;
+    private String sysUserPhone;
+    private String sysUserIdCard;
+    private Integer sysUserCardType;
+    private String sysUserEmail;
+    private String sysUserQq;
+    private String sysUserWx;
+    private Integer sysUserLock;
+    private Date sysLockDate;
+    private Integer sysUserExpired;
+    private Date sysExpiredDate;
+    private Integer sysUserType;
+    private Integer sysUserSort;
 }
